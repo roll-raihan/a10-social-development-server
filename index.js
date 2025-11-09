3d843b2f13abae0039b066da0eafa3c2cc155792
roll-raihan <raihanahmednasim@gmail.com>
2025-11-09 10:10:01 +0600

    dotenv added

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n2kdiwk.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 
 app.get('/', (req, res) => {
     res.send('Social development server is running!!');
 })
 
+async function run() {
+    try {
+        await client.connect();
+        
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+       
+    }
+}
+run().catch(console.dir);
+
 app.listen(port, () => {
     console.log(`Social server is running on port: ${port}`);
 })
+// qGUH4M6H26CWkd0m
