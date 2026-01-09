412f3e37c22cdef673532df56bbc6f5f5d9a3df4
roll-raihan <raihanahmednasim@gmail.com>
2026-01-09 17:20:20 +0600

    uncomment client.connection

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // await client.connect();
+        await client.connect();
 
         const db = client.db('social_db');
         const treesCollection = db.collection('trees');
